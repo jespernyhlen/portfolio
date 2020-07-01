c44bb373177bbf6e7769bebe2f633b814fa9cfd9
jespernyhlen <jeppe_nyhlen@hotmail.com>
2020-07-01 17:34:01 +0200

    Adding base structure for project

---
+++ src/app.js
@@ -0,0 +1,12 @@
+async function App() {
+    const template = document.createElement('template');
+
+    template.innerHTML = `
+    <div class="content-wrapper">
+    </>
+  `;
+    // Return a new node from template
+    return template.content.cloneNode(true);
+}
+
+export default App;
